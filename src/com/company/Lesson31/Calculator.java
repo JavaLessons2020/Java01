f352cab298878efd31bf18c0d89942b2a16fb3be
Evgeliy Lukas <lukashik_e@itstep.academy>
2020-07-11 12:33:45 +0300

    Add Lesson 31

---
+++ src/com/company/Lesson31/Calculator.java
@@ -0,0 +1,7 @@
+package com.company.Lesson31;
+
+public class Calculator {
+    public int sum(int a, int b){
+        return a+b;
+    }
+}
---
+++ src/com/company/Lesson31/Run.java
@@ -0,0 +1,9 @@
+package com.company.Lesson31;
+
+public class Run  extends Object{
+
+    public static void main(String[] args) {
+        Test test = new Test();
+        test.test();
+    }
+}
